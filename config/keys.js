115493332dd70011162d51fe0af5661131bf56cc
Ctracey92 <cody.tracey92@gmail.com>
2020-09-21 12:00:05 -0500

    starting to set up the basic server, and back end for my app

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbAdmin:RDEFlMuNLQcLAhny@cluster0.57iyc.mongodb.net/version2?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Bodyparses middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+
+//Database configuration
+const db = require("./config/keys").mongoURI;
+
+//Conect to MongoDb
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB successfully connected");
+    })
+    .catch((err) => console.log(err));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is up and running on ${port} !`));
