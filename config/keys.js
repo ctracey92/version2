96ccf9ea4ffbdb4a0aafbf2a4431a5233163d68f
Ctracey92 <cody.tracey92@gmail.com>
2020-09-21 13:15:35 -0500

    added in the base for passport

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
 module.exports = {
-    mongoURI: process.env.REACT_APP_URI
+    mongoURI: process.env.REACT_APP_URI,
+    secretOrKey: process.env.REACT_APP_SECRETORKEY
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,31 @@ const validateLoginInput = require("../../validation/login");
 
 //Load User model
 const User = require("../../models/User");
+const { errors, isValid } = validateRegisterInput(req.body);
+
+//Check Validation
+if (!isValid) {
+    return res.staus(400).json(errors);
+}
+
+User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+        return res.stats(400).json({ email: "Email already exists" });
+    } else {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        //Hash password before saving it
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                    .then(user => res.jason(user))
+                    .catch(err => console.log(err));
+            });
+        });
+    }
+});
