278a47628cb4f7253e9a9940f33f19de10450dc4
Ctracey92 <cody.tracey92@gmail.com>
2020-12-03 21:58:08 -0600

    more styling work

--- client/src/components/Pages/Landing.js
+++ client/src/components/Pages/Landing.js
@@ -1,27 +1,21 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Button} from 'reactstrap';
 
 const Landing = props => {
     return(
-        <div>
-            <Container>
-                <Row>
+        // <div>
+            <Container className='align-items-center'>
+                <Row className="d-flex justify-content-center">
                     <h1>Welcome to my page!</h1>
                 </Row>
-                <Row>
-                    <p>Please excuse the mess as we are currently under renovations.</p>
-                </Row>
-                <Row>
-                    <Col>
-                        <Link to="/login">Log In</Link>
-                    </Col>
-                    <Col>
-                        <Link to="/register">Register</Link>
-                    </Col>
+                <Row className="d-flex justify-content-center">
+                    <button className="mr-1 ml-1 main-bttn"><Link to="/login">Log In</Link></button>
+                    <button className="mr-1 ml-1 main-bttn"><Link to="/register">Register</Link></button>
+
                 </Row>
             </Container>
-        </div>
+        // </div>
     )
 }
 
